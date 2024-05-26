d31315a94111118d02ba373f465219e16d136c08
jhon <ortizz@gmail.com>
2024-05-25 20:02:59 -0500

    base url c

--- src/api/Restaurante.api.js
+++ src/api/Restaurante.api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Definir baseURL según el entorno
 const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://your-production-backend-url.com' 
+  ? 'https://electiva-b.azurewebsites.net' 
   : 'http://127.0.0.1:8000'; // URL de desarrollo
 
 console.log(`API URL: ${baseURL}/api/restaurante/api_backen/`);
