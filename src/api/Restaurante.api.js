bfd6e582003564854fd71f29ed8774cf2e83d308
jhon <ortizz@gmail.com>
2024-05-25 19:42:07 -0500

    base url

--- src/api/Restaurante.api.js
+++ src/api/Restaurante.api.js
@@ -1,7 +1,11 @@
-import axios from 'axios'; 
+import axios from 'axios';
 
 const baseURL = process.env.NODE_ENV === 'production' ? 
-                import.meta.env.VITE_BACKEND_URL : 'https://electiva-b.azurewebsites.net/';
+                import.meta.env.VITE_BACKEND_URL : 'https://electiva-b.azurewebsites.net';
+
+if (!baseURL) {
+    throw new Error("Base URL is not defined. Please check your environment variables.");
+}
 
 console.log(`API URL: ${baseURL}/api/restaurante/api_backen/`);
 
